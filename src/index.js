da17a0c4a30953e72049358f9a546ec835e007f4
EiKayKhaing2024 <eikkhaing.ekk@gmail.com>
2024-10-21 00:43:49 +0630

    added the java script function to the original project,

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert("Hello World");
+function generate(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let poemgeneratorElement = document.querySelector("#poem-generator");
+poemgeneratorElement.addEventListener("submit", generate);
